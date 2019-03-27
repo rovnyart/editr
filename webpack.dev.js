b80eb0f9843c27c7dacee15bd76a4e7ce3a29922
rovnyart <artur.kozulia@gmail.com>
2019-03-28 00:15:20 +0300

    initial

---
+++ webpack.dev.js
@@ -0,0 +1,12 @@
+const merge = require('webpack-merge');
+
+const common = require('./webpack.common');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devServer: {
+    host: 'localhost',
+    port: 8888,
+    open: true,
+  },
+});
